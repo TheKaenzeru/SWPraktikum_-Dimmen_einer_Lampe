93143b6fc881bb0b26545ae325e2730d60e68cee
Lemmy <lemmy_paul.schreiber@mailbox.tu-dresden.de>
2026-06-04 11:47:07 +0200

    5.3 Testen des Spannungs Null Durchgang

--- src/main.cpp
+++ src/main.cpp
@@ -3,18 +3,37 @@
 int taster1 = 0;
 int taster2 = 1;
 int led = 14;
+int gate = 3;
+int SND = 6;
+int haeufigkeit = 50;
+
+bool lampe = false;
+int counter = 0;
+bool wait= false;
 
 void setup(){
   pinMode(led, OUTPUT);
   pinMode(taster1, INPUT);
   pinMode(taster2,INPUT);
+  pinMode(gate, OUTPUT);
 }
 void loop(){
-  if (digitalRead(taster1) == HIGH){
-    digitalWrite(led, HIGH);
+  if (digitalRead(SND)==HIGH && wait == false){
+    counter++;
+    wait = true;  
+  } else if (digitalRead(SND)== LOW && wait == true){
+    wait = false;
   }
-  else if (digitalRead(taster2) == HIGH){
-    digitalWrite(led, LOW);
+
+  if (counter >= haeufigkeit){
+    counter = 0;
+    lampe = !lampe;
   }
-delay(100);
+
+  if (lampe){
+    digitalWrite(gate, HIGH);
+  } else {
+    digitalWrite(gate,LOW);
+  }
+
 }
